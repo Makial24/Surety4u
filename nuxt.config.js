49c75c8c35422d9c0c0d937cdc418f70e2704139
makial24 <you@example.com>
2022-05-29 19:52:24 +0800

    First upload

---
+++ nuxt.config.js
@@ -0,0 +1,64 @@
+export default {
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    title: 'Surety4u',
+    htmlAttrs: {
+      lang: 'en'
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+      { name: 'format-detection', content: 'telephone=no' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [
+  ],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [
+  ],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+    // https://go.nuxtjs.dev/eslint
+    '@nuxtjs/eslint-module',
+    '@nuxtjs/tailwindcss'
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+    // https://go.nuxtjs.dev/axios
+    '@nuxtjs/axios'
+  ],
+
+  firebase: {
+    config: {
+      apiKey: "AIzaSyBY6abPx0DG6C_QXvwm9WbhZiZY8mBPf_k",
+      authDomain: "ztmburger-b676d.firebaseapp.com",
+      projectId: "ztmburger-b676d",
+      storageBucket: "ztmburger-b676d.appspot.com",
+      messagingSenderId: "341965058139",
+      appId: "1:341965058139:web:b21d1b050e5515f6f14f7e",
+      measurementId: "G-0QC1XHYF28"
+    },
+  },
+
+  // Axios module configuration: https://go.nuxtjs.dev/config-axios
+  axios: {
+    // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
+    baseURL: '/'
+  },
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {
+  }
+}
---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <div id="header" class="grid grid-cols-2">
+      <div id="left" class="pl-20 t-5">
+        <img src="logo.jpeg" class="w-[100px]" alt="">
+      </div>
+      <div id="right" class="mt-8">
+        <li>
+          <img src="Whatsapp.jpg" class="w-[80px]" alt="">
+        </li>
+        <li>
+          <img src="telegram.jpg" class="w-[80px]" alt="">
+        </li>
+        <li>0162060180</li>
+      </div>
+    </div>
+  </div>
+</template>
